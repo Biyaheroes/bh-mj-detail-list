b5fe5fe675c27b1578bd118b10960729719dca26
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-07-28 04:45:52 +0800

    Update bh-mj-detail-list.

---
+++ component.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import DetailList from "./detail-list.js";
+
+ReactDOM.render( 
+	<DetailList
+		list="W3sidGl0bGUiOiJIZWxsbyIsInZhbHVlIjoid29ybGQifSx7InRpdGxlIjoiSGVsbG8iLCJ2YWx1ZSI6IndvcmxkIn0seyJ0aXRsZSI6IkxvcmVtIElwc3VtIiwidmFsdWUiOiJIZWxsbyBXb3JsZCJ9LHsidGl0bGUiOiJIb2xhIEFtaWdvIiwidmFsdWUiOiJIZWxsbyBXb3JsZCBZZWFoIn1d"
+		>
+		</DetailList>,
+		
+		document.getElementById( "root" )
+	);
